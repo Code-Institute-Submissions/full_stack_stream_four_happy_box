376a5aa57fff4c06bebca2540d5f7c41c576fb3c
Eva Larkin <larkineva@gmail.com>
2018-07-05 10:19:49 +0000

    initial commit

---
+++ ecommerce/settings/local.py
@@ -0,0 +1,30 @@
+from .base import *
+
+SECRET_KEY='t&rn%2^#u*1#+1_i4-uohtlsy$9h1$h+cxdu$vdhrdym$6=(bz'
+
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SYSTEM_EMAIL = 'admin@example.com'
