83647288ae1a7b6cfbeb48d2ab2351d140032e4f
Eva Larkin <larkineva@gmail.com>
2018-08-02 09:24:44 +0000

    multiple image functionality on product detail page added & fancy box gallery

--- products/views.py
+++ products/views.py
@@ -8,17 +8,17 @@ from reviews.models import Review
 def product_list(request):
     products = Product.objects.all()
     categories = Category.objects.all()
-    images = Image.objects.all()
     wishlist = request.session.get('wishlist', {})
     in_wishlist = id in wishlist
-    return render(request, 'products/product_list.html', {'products':products,'categories':categories, 'images':images, 'in_wishlist': in_wishlist})
+    return render(request, 'products/product_list.html', {'products':products,'categories':categories, 'in_wishlist': in_wishlist})
     
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
+    images = Image.objects.filter(product__name=product)
     form = ReviewForm()
     wishlist = request.session.get('wishlist', {})
     in_wishlist = id in wishlist
-    return render(request, 'products/product_detail.html', {'product':product, 'review_form': form, 'in_wishlist': in_wishlist})
+    return render(request, 'products/product_detail.html', {'product':product, 'images':images, 'review_form': form, 'in_wishlist': in_wishlist})
     
 def get_cat_products(request, category):
     products = Product.objects.filter(category__name=category)
